f09183ec5bbb65c53e7cd575890cfaf7fc8f7519
Shubhayu Majumdar <137767634+shubhayu-majumdar-bv@users.noreply.github.com>
2024-06-29 03:10:35 +0530

    add: yf client

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
+from pprint import pprint
 from Tank.base import APIClientFactory, load_config
+from Tank.yahoofinance_client import YahooFinanceAPIClient
 
 
 def main():
     # Load configuration
     config = load_config('config.yaml')
-    print(config)
+
+    # Initialize APIClientFactory
+    client_factory = APIClientFactory()
+    
+    # Register YahooFinanceAPIClient manually
+    yahoo_finance_client = YahooFinanceAPIClient()
+    client_factory.register_client('yahoo_finance', yahoo_finance_client)
+
+    # Fetch live data for GOOGL using the registered client
+    client = client_factory.get_client('yahoo_finance')
+    nike_data = client.fetch_live_data('NKE')
+
+    # nike_info = client.fetch_info('NKE')
+    pprint(nike_data)
 
 main()
